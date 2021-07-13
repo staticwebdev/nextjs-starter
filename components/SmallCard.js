4c4bc8d6bc5da43e895fef3f4ba897eb2b664308
Russell Conard <ruconard@microsoft.com>
2021-07-13 16:14:59 -0400

    Update the starter project to include Next.js static generation methods

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function SmallCard({ Icon, title, slug }) {
   return (
-    <a className="card-small" href={`/project/${slug}`}>
-      <Icon w={153} h={163} />
-      <h3>{title}</h3>
-    </a>
+    <Link
+      href={`/project/${slug}`} >
+      <a
+        className="card-small" >
+        <Icon w={153} h={163} />
+        <h3>{title}</h3>
+      </a>
+    </Link>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
+import Head from 'next/head';
 import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 
-import { projects } from '../utils/projectsData';
-
-const Home = () => (
+const Home = (props) => (
   <div className="home">
+    <Head>
+      <title>Home | Next.js Starter Static Apps</title>
+    </Head>
     <h1>What Can I Deploy to Static Apps?</h1>
     <div className="card-grid">
-      {projects.map((project) => {
+      {props.projects.map((project) => {
         const Icon = projectIcons[project.id];
         return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
       })}
     </div>
   </div>
 );
+
+// At build time, generate a static version of the page
+export async function getStaticProps(context) {
+  const { projects } = require('../utils/projectsData.js');
+
+  return {
+    props: {
+      projects,
+    },
+  };
+};
 
 export default Home;
