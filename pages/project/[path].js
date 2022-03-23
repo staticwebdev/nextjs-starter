657e4718f96575c6cebcf45d1bb93cc3ce66f561
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-03-23 09:02:03 -0400

    chore: add missing GitHub stats

--- pages/project/[path].js
+++ pages/project/[path].js
@@ -81,6 +81,11 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const project = projects.find(proj => proj.slug === params.path);
+  const res = await fetch(`https://api.github.com/repos/${project.path}`);
+  const data = await res.json();
+  project.open_issues = data.open_issues;
+  project.subscribers_count = data.subscribers_count;
+  project.stargazers_count = data.stargazers_count;
   return { props: { project } };
 }
 
